53bdca46d19df49d5a723bc1a69d0cf36dad8cf9
Makram Kamaleddine <makram.kd@gmail.com>
2015-12-05 12:01:52 -0500

    adding timer events: prediction takes a lot less time than fitting (as expected)

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -34,12 +34,23 @@ features_train, features_test, labels_train, labels_test = preprocess()
 # create the classifier object
 clf = GaussianNB()
 
+# start the timer
+t0 = time()
+
 # fit it to the data / train it on the data
 clf.fit(features_train, labels_train)
+
+# print the training time
+print "training time:", round(time() - t0, 3), "s"
+
+# start the timer again for getting prediction time
+t1 = time()
 
 # get a prediction on some data
 pred = clf.predict(features_test)
 
+# print the prediction time
+print "prediction time:", round(time() - t1, 3), "s"
 # get the accuracy of the prediction
 accuracy = accuracy_score(labels_test, pred)
 #########################################################

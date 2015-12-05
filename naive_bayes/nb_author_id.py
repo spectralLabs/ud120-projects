66c56cdea415cc11fd579d352f21deceb944b884
Makram Kamaleddine <makram.kd@gmail.com>
2015-12-05 11:56:18 -0500

    finishing up the naive bayes prediction for the first lesson

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -15,6 +15,10 @@ from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
 
+# get the Gaussian naive bayes classifier object
+from sklearn.naive_bayes import GaussianNB 
+# import the accuracy module
+from sklearn.metrics import accuracy_score
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
@@ -27,7 +31,18 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
+# create the classifier object
+clf = GaussianNB()
 
+# fit it to the data / train it on the data
+clf.fit(features_train, labels_train)
+
+# get a prediction on some data
+pred = clf.predict(features_test)
+
+# get the accuracy of the prediction
+accuracy = accuracy_score(labels_test, pred)
 #########################################################
 
-
+# print the accuracy to see it
+print accuracy
